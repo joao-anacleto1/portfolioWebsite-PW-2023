e40dcd4c887255f55cb784a3f211afaa4086755e
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2023-04-27 12:16:19 +0100

    faz onmouseover e onmouse out

---
+++ www/onmouse.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+    alert("Clica no botão e escreve uma frase inspiracional quando olhas para a imagem que está acima!");
+  }
+  
+  function esconderMensagem() {
+    alert("Obrigado!");
+  }
