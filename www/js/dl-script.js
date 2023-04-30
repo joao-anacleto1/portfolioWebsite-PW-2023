2ca3e3089e3d73685e6e817d961be8da4a893eea
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2023-04-30 13:00:26 +0100

    adiciona local storage no script dark/light mode

--- www/js/dl-script.js
+++ www/js/dl-script.js
@@ -5,37 +5,51 @@ const body = document.querySelector("body"),
   siderbarOpen = document.querySelector(".siderbarOpen"),
   siderbarClose = document.querySelector(".siderbarClose");
 
-var darkmodeData = true;
+// Verificar se há uma entrada "darkmode" no LocalStorage
+let darkmodeData = localStorage.getItem("darkmode");
+if (darkmodeData === null) {
+  darkmodeData = true;
+} else {
+  darkmodeData = JSON.parse(darkmodeData);
+}
 
-window.addEventListener("DOMContentLoaded", () => {
+// Definir o modo escuro com base no valor do LocalStorage
+if (darkmodeData) {
   body.classList.add("dark");
-  modeToggle.classList.toggle("active");
-});
+  modeToggle.classList.add("active");
+} else {
+  body.classList.remove("dark");
+}
 
-//java script code to toggle dark and light mode
+// Adicionar um event listener para o botão de alternar o modo escuro
 modeToggle.addEventListener("click", () => {
   modeToggle.classList.toggle("active");
 
+  // Alternar o valor da variável darkmodeData
+  darkmodeData = !darkmodeData;
+
+  // Atualizar a classe do body com base no novo valor de darkmodeData
   if (darkmodeData) {
-    body.classList.remove("dark");
-  } else {
     body.classList.add("dark");
+  } else {
+    body.classList.remove("dark");
   }
 
-  darkmodeData = !darkmodeData;
+  // Armazenar o novo valor de darkmodeData no LocalStorage
+  localStorage.setItem("darkmode", JSON.stringify(darkmodeData));
 });
 
-//java script code to toggle search box
+// Adicionar um event listener para o botão de alternar a caixa de pesquisa
 searchToggle.addEventListener("click", () => {
   searchToggle.classList.toggle("active");
 });
 
-//java script code to toggle siderbar
-
+// Adicionar um event listener para o botão de alternar a barra lateral
 siderbarOpen.addEventListener("click", () => {
   nav.classList.add("active");
 });
 
+// Adicionar um event listener para o body para fechar a barra lateral ao clicar fora dela
 body.addEventListener("click", (e) => {
   let clickedElm = e.target;
 
